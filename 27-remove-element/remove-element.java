0c1e889cf9e71b0896c6609e28ac46e2eca8d096
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2025-05-20 11:29:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (92.55%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,25 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int left=0, right=nums.length-1, k=0;
-        if(!Arrays.stream(nums).findAny().isPresent()) return 0;
-        while(left<=right)
-        {
-            if(nums[left]==val && nums[right]!=val)
-            {
-                nums[left]=nums[right];
-                left++;
-                right--;k+=1;
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
             }
-            else if( nums[right]==val)
-            {
-                right--;k+=1;
-            }
-            else if( nums[left]!=val)
-            {
-                left++;
-            }
-            
         }
-        return nums.length-k;
+
+        return k;        
     }
 }
