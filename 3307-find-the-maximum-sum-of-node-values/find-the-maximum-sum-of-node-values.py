7169de2738dd055bdecd5c17148777d1e9298708
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2025-05-23 12:01:00 +0530

    Time: 58 ms (0.00%) | Memory: 20.7 MB (100.00%) - LeetSync

---
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maximumValueSum(self, nums, k, edges):
+        N = len(nums)
+        #if flipping x, this is the benefit
+        delta = []
+        for x in nums:
+            delta.append((x^k) - x)
+        delta.sort(reverse=True)
+        total = sum (nums)
+        for i in range(0, N, 2):
+            if i+1 >= N:
+                continue
+            a, b = delta[i], delta[i+1]
+            if a+b > 0:
+                total += (a + b)
+        return total
