ac305b1656a0caa51851fd080cf1a08cc61cbb05
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2026-01-19 21:07:31 +0530

    Time: 5 ms (72.88%) | Memory: 45.9 MB (78.11%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        else if(x >= 0 && x < 10)
+        {
+            return true;
+        }
+        else
+        {
+            int rev = 0, n = x;
+            while(x>0)
+            {
+                int r = x%10;
+                rev = (rev*10) + r;
+                x/=10;
+            }
+            if(n==rev)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
