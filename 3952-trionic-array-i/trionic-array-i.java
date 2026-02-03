e280ca10e25ea48af89589a0712598840a01798f
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2026-02-03 20:01:17 +0530

    Time: 1 ms (72.27%) | Memory: 44.7 MB (64.71%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -1,26 +1,29 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
-        int count = 0;
-        boolean flag = false;
-        if(nums[0] >= nums[1]) return false;
-        for(int i=2;i<nums.length;i++)
-        {
-            if(flag == false && nums[i]<nums[i-1])
-            {
-                flag = true;
-                count++;
-            }
-            if(flag == true && nums[i]>nums[i-1])
-            {
-                flag = false;
-                count++;
-            }
-            if(nums[i] == nums[i-1]) return false;
-            if(count > 2)
-            {
+        int flag = 0;
+        if(nums.length < 4)
                 return false;
+        for(int i=1;i<nums.length;i++)
+            {
+                if(nums[i]==nums[i-1]) return false;
+                if(flag == 0 && nums[i]<nums[i-1])
+                {
+                    flag++;
+                }
+                else if(flag==1 && nums[i-1]<nums[i])
+                {
+                    flag++;
+                }
+                else if(nums[0]>=nums[1])
+                {
+                    return false;
+                }
+                else if(flag == 2 && nums[i]<nums[i-1])
+                    return false;
             }
-        }
-        return count == 2 ? true : false;
+        if(flag==2)
+            return true;
+        else
+            return false;
     }
 }
