7cdb29e3da07f717f8c9d89e71a9790ce7eec97a
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2026-06-14 14:22:37 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (90.46%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.java
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -1,7 +1,8 @@
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
         char[] m = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
-        StringBuilder ans = new StringBuilder();
+        char[] ans = new char[words.length];
+        int i=0;
         for(String w :  words)
         {
             int weight = 0;
@@ -9,8 +10,8 @@ class Solution {
             {
                 weight += weights[c - 97];
             }
-            ans.append(m[weight%26]);
+            ans[i++] = (m[weight%26]);
         }
-        return ans.toString();
+        return new String(ans);
     }
 }
