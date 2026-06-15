1a56e821ba6036552513fc00f4e75c6662ef2610
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2026-06-15 19:24:14 +0530

    Time: 2 ms (88.52%) | Memory: 47 MB (99.71%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums.length < 2)
+        {
+            return true;
+        }
+        int n = nums.length-1;
+        int prev = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] == 0 && prev == 0)
+            {
+                return false;
+            }
+            if(i + nums[i] >= n)
+            {
+                return true;
+            }
+            prev = prev < nums[i] ? nums[i] : prev;
+            prev--;
+        }
+        return true;
+    }
+}
