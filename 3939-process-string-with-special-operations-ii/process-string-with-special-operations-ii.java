2f7be07560a4cb4ad753504ec7b87d80f8c17497
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2026-06-17 22:35:46 +0530

    Time: 32 ms (26.92%) | Memory: 48 MB (26.92%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
@@ -0,0 +1,59 @@
+class Solution {
+    public char processStr(String s, long k) {
+        long length = 0;
+        for(char c : s.toCharArray())
+        {
+            switch(c)
+            {
+                case '*':
+                    if(length > 0) length--;
+                    break;
+
+                case '#':
+                    length *= 2;
+                    break;
+                
+                case '%':
+                    break;
+                
+                default:
+                    length++;
+                    break;
+            }
+        }
+        System.out.print(length);
+        if(k+1 > length)
+        {
+            return '.';
+        }
+        for(int i=s.length()-1; i>=0; i--)
+        {
+            char c = s.charAt(i);
+            switch(c)
+            {
+                case '*':
+                    length += 1;
+                    break;
+                case '#':
+                    long half = (length+1) / 2;
+                    if(k+1 > half)
+                    {
+                        k -= (length/2);
+                    }
+                    length = half;
+                    break;
+                case '%':
+                    k = length - k - 1;
+                    break;
+                default:
+                    if(k+1 == length)
+                    {
+                        return c;
+                    }
+                    length-=1;
+                    break;
+            }
+        }
+        return '.';
+    }
+}
