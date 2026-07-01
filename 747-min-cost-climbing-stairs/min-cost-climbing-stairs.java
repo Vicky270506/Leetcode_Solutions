43a2bb5b181d5571b3fa7db543a47fafd37de434
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2026-07-02 00:09:19 +0530

    Time: 1 ms (39.41%) | Memory: 44.7 MB (75.91%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -6,6 +6,6 @@ class Solution {
             if(i+2 < n)
                 cost[i] = cost[i] + Math.min(cost[i+1], cost[i+2]);
         }
-        return Math.min(cost[0], cost[1]);
+        return cost[1] < cost[0] ? cost[1] : cost[0];
     }
 }
