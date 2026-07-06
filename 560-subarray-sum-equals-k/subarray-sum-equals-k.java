bfc893c406fc5a830a3534ceeb8c0acca2217142
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2026-07-07 00:54:25 +0530

    Time: 24 ms (76.97%) | Memory: 48.7 MB (59.15%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,23 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        
-        int ans=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i] == k)
-            {
-                ans++;
-            }
-                int sum = nums[i];
-                for(int j=i+1;j<nums.length;j++)
-                {
-                    sum += nums[j];
-                    if(sum == k)
-                    {
-                        ans++;
-                    }
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Base case: one prefix sum of 0 exists before the array starts
+        map.put(0, 1);
+
+        int sum = 0;
+        int ans = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            // Check if there is a previous prefix sum such that
+            // currentSum - previousSum = k
+            ans += map.getOrDefault(sum - k, 0);
+
+            // Store/update the current prefix sum
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
+
         return ans;
     }
 }
