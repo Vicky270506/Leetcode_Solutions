b724b238e4528ecc9ff510ce32c458eb1d67f61d
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2026-08-02 06:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (11.56%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,12 +1,16 @@
+from functools import lru_cache
+
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
         self.nums = nums
-        return self.score(0, len(nums) - 1) >= 0
 
-    def score(self, left: int, right: int) -> int:
-        if left > right:
-            return 0
-        return max(
-            self.nums[left]  - self.score(left + 1, right),
-            self.nums[right] - self.score(left, right - 1)
-        )
+        @lru_cache(None)
+        def score(left, right):
+            if left > right:
+                return 0
+            return max(
+                nums[left]  - score(left + 1, right),
+                nums[right] - score(left, right - 1)
+            )
+
+        return score(0, len(nums) - 1) >= 0
