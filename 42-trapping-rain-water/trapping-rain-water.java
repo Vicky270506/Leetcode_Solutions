be4f85b67cfc44a5e04bf2c8bf0eb9aa31e409a4
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2026-08-05 00:09:10 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (92.02%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,28 +1,29 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int prefix = height[0];
-        int[] suffix = new int[n];
-        suffix[n-1] = height[n-1];
+        int l = 0, r = height.length-1;
+        int lmax = 0, rmax = 0, total = 0;
 
-        for(int i=n-2;i>=0;i--)
+        while(l < r)
         {
-            suffix[i] = Math.max(suffix[i+1],height[i]);
-        }
-        //System.out.println(Arrays.toString(prefix));
-        //System.out.println(Arrays.toString(suffix));
-        int total = 0;
-        for(int i=0; i<n;i++)
-        {
-            int h = height[i];
-            if(h <= prefix && h <= suffix[i])
-            {
-                total += (Math.min(prefix, suffix[i]) - h);
+            if(height[l] <= height[r]){
+                if(lmax < height[l]){
+                    lmax = height[l];
+                }
+                else{
+                    total += lmax - height[l];
+                }
+                l++;
             }
-            prefix = prefix > height[i] ? prefix : height[i];
+            else{
+                if(rmax < height[r]){
+                    rmax = height[r];
+                }
+                else{
+                    total += rmax - height[r];
+                }
+                r--;
+            }
         }
-    
         return total;
-
     }
 }
