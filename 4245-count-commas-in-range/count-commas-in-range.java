7234261d8dc5046418845168c0dc0e1d06cc9558
Vignesh <114830700+Vicky270506@users.noreply.github.com>
2026-09-08 21:52:09 +0530

    Time: 1 ms (83.65%) | Memory: 42.8 MB (8.32%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countCommas(int n) {
+       String s = Integer.toString(n);
+       int l = s.length() > 3 ? s.length() : 0;
+       if(l==0){
+        return l;
+       }
+       else{
+        return n-999;
+       }
+    }
+}
